6f0dcf54df2654267b3669aa23481b11d9a7e5c2
olga-forforce <olga.pererva+qa@forforce.com>
2021-09-30 22:49:20 +0300

    diploma

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Items, ButtonAdd, Name } from './AddItems.module.scss';
+import React, { useState} from 'react';
+import { Items, ButtonAdd, NameItem } from './AddItems.module.scss';
 
 function AddItems () {
+    const [name, setName] = useState('');
+
+    const handleChange = (event) => {
+        if (event.target.value.length !== 50) {
+            setName(event.target.value)
+        }
+    }
     return (
         <div className={Items}>
-            <input className={Name} id="name" type="text" placeholder="Name..." />
+            <input
+                value={name}
+                onChange={handleChange}
+                className={NameItem} id="name" type="text" placeholder="Name..." />
             <input className={ButtonAdd} type="submit" value="Add Item" />
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { NameForm, AppHeader, Search } from './Header.module.scss';
 
 function Header() {
+
   return (
     <div className={AppHeader}>
       <header className={NameForm}>
         <p>
          To Do List
         </p>
-          <input className={Search} id="name" type="text" placeholder="search" />
+          <input
+              className={Search} id="name" type="text" placeholder="search" />
       </header>
     </div>
   );
