f5ce41016a06e1d0a0b68092cd504ba2968ef673
olga-forforce <olga.pererva+qa@forforce.com>
2021-10-05 14:43:47 +0300

    'diploma'

--- src/ui/components/Items/Items.js
+++ src/ui/components/Items/Items.js
@@ -7,15 +7,19 @@ export default function Items () {
     const [elem, setElem] = useState([{ id: 1, text:'Do JavaScript Projects'},
         {id: 2, text:'Learn NodeJS'}, {id: 3, text:'Learn ReactJS'}, {id: 4, text:'Learn GraphQL'}]);
 
-    {elem.map(() => {
-    return (
-        <>
-        <div className={Item}>
-            <p className={List}>{elem}</p>
-            <div className={Emoji}><p>⭐</p><p>✔️</p><p>✏️</p><p>❌</p></div>
-        </div>
-            <Button name="Clear Items"/>
-        </>
-        )})
+    {
+        elem.map((item) => {
+            console.log(item)
+            return (
+                <>
+                    <div className={Item}>
+                        <p className={List}>{item.text}</p>
+                        <div className={Emoji}><p>⭐</p><p>✔️</p><p>✏️</p><p>❌</p></div>
+                    </div>
+                    <Button name="Clear Items"/>
+                </>
+            )
+        })
     }
+
 }
